053413f0638437d50ebe6ed8e0e22644bda76d07
sarah-knowles <sjk339@gmail.com>
2021-02-14 16:42:58 +1300

    complete

--- functions.js
+++ functions.js
@@ -1,1 +1,34 @@
 //filter, find, map
+
+function find (arr, func) {
+  for(let item of arr) {
+    if(func(item)) {
+      return item
+    }
+  }
+}
+
+function filter (arr, func) {
+  let newArr = []
+  for(let item of arr) {
+    if(func(item)) {
+     newArr.push(item)
+    }
+  }
+  return newArr
+}
+
+
+function map (arr, func) {
+  let newArr = []
+  for(let item of arr) {
+    newArr.push(func(item))
+  }
+  return newArr
+}
+
+module.exports = {
+  find,
+  filter,
+  map
+}
